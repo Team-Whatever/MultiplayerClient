78821b773430d194d43a488143e451e0d816be00
Ben <34342294+LeScoops@users.noreply.github.com>
2020-02-16 13:08:59 -0500

    Added Coloured Ramp & Wall

---
+++ Assets/Scripts/WallColour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallColour : MonoBehaviour
+{
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            child.GetComponent<MeshRenderer>().material.color = new Color(UnityEngine.Random.Range(0.0f, 1.0f), 
+                                                                        UnityEngine.Random.Range(0.0f, 1.0f), 
+                                                                        UnityEngine.Random.Range(0.0f, 1.0f));
+        }            
+    }
+}
